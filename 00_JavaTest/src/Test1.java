b0b6428d62ad4032634b84901cf294d666259e59
suveen01 <suveen01@naver.com>
2025-01-20 11:25:31 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,27 @@
+// JS와 주석 도잉ㄹ
+/* Run 버튼 클릭 시 (단축기 ctrl + f11)
+1) 자동으로 컴파일 수행(javac ooo.java)
+ -> .class 파일(byte code) 생성
+
+2) 컴파일 완료 후 .class 파일 실행(java class 파일명)
+*/
+
+
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("이클립스 실행중");
+		
+//		Java API DOC
+//		 -> JAVA에서 제공해주는 코드의 사용법이  설명 되어있는 문서
+//		DOC 확인 방법
+//		1) google -> java 21 api 검색
+//		
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,11 @@
+ public class JavaTest{
+
+   public static void main(String[] args){
+      System.out.println("Hello World!");
+      
+
+   for(int i=1 ; i<=5 ; i++){
+      System.out.println(i+"번째 줄 출력");	
+   }	
+ } 
+}
